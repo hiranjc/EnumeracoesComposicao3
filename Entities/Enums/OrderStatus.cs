67746e822ba00ddad1e13bee82d31cf4680ab4e7
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:28:14 -0300

    first commit

---
+++ Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace EnumeracoesComposicao3.Entities.Enums
+{
+    internal enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
